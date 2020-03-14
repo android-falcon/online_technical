8954ef0848bec7c534db66eabb3dc288c2b3b3e6
abeer <sundos.tamomo20@gmail.com>
2020-03-14 13:42:08 +0200

    Abeer ^_^
    V1

---
+++ app/src/main/java/com/falconssoft/onlinetechsupport/LoginActivity.java
@@ -0,0 +1,66 @@
+package com.falconssoft.onlinetechsupport;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Dialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText id;
+    private Button done;
+    private ImageView settings;
+    int employeeID;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        settings = findViewById(R.id.login_settings);
+
+        settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Dialog dialog = new Dialog(LoginActivity.this);
+                dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                dialog.setContentView(R.layout.settings_dialog);
+
+                id = dialog.findViewById(R.id.settings_id);
+                done = dialog.findViewById(R.id.settings_done);
+
+                  employeeID = 0;
+
+                done.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (!TextUtils.isEmpty(id.getText().toString())){
+                            employeeID = Integer.parseInt(id.getText().toString());
+
+                            switch (employeeID){
+                                case 1:// manager
+                                    break;
+                                case 2:// call center
+                                    break;
+                                case 3:// online
+                                    Intent intent3 = new Intent(LoginActivity.this, OnlineActivity.class);
+                                    startActivity(intent3);
+
+                                    break;
+                            }
+                        }
+                        dialog.dismiss();
+                    }
+                });
+                dialog.show();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/falconssoft/onlinetechsupport/OnlineActivity.java
@@ -0,0 +1,85 @@
+package com.falconssoft.onlinetechsupport;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+//import android.support.design.widget.Snackbar;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+
+public class OnlineActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ConstraintLayout coordinatorLayout;
+    private CircleImageView profilePicture;
+    private ImageView breakButton, exitButton;
+    private Button exitBreak;
+    private FloatingActionButton addactionButton;
+    private Snackbar snackbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_online);
+
+        profilePicture = findViewById(R.id.online_settings);
+        breakButton = findViewById(R.id.online_break);
+        exitButton = findViewById(R.id.online_exit);
+        coordinatorLayout = findViewById(R.id.online_cordinator_layout);
+        addactionButton = findViewById(R.id.online_add);
+
+        addactionButton.setOnClickListener(this);
+        breakButton.setOnClickListener(this);
+        exitButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()) {
+            case R.id.online_add:
+                snackbar = Snackbar.make(coordinatorLayout, Html.fromHtml("<font color=\"#3167F0\">Checked out Successfully</font>"), Snackbar.LENGTH_SHORT);
+                View snackbarLayout = snackbar.getView();
+                TextView textViewSnackbar = snackbarLayout.findViewById(com.google.android.material.R.id.snackbar_text);
+                textViewSnackbar.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_check, 0, 0, 0);
+                textViewSnackbar.setCompoundDrawablePadding(15);
+                snackbar.show();
+                break;
+            case R.id.online_break:
+                profilePicture.setBorderColor(getResources().getColor(R.color.yellowf));
+                final Dialog dialog = new Dialog(this);
+                dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                dialog.setContentView(R.layout.break_dialog);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.setCanceledOnTouchOutside(false);
+
+                exitBreak = dialog.findViewById(R.id.breakDialog_exit);
+                exitBreak.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        profilePicture.setBorderColor(getResources().getColor(R.color.greenf));
+                        dialog.dismiss();
+                    }
+                });
+
+                dialog.show();
+                break;
+            case R.id.online_exit:
+                profilePicture.setBorderColor(getResources().getColor(R.color.redf));
+                break;
+        }
+    }
+}
