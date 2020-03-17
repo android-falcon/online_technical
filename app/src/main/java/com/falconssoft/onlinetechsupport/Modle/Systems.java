78198afcafea9fa390b3ddeebfbbfebceb0e2b50
Tahani Ghanoum <tahanighanoum1234@gmail.com>
2020-03-17 12:19:33 +0200

    online layout

---
+++ app/src/main/java/com/falconssoft/onlinetechsupport/Modle/Systems.java
@@ -0,0 +1,25 @@
+package com.falconssoft.onlinetechsupport.Modle;
+
+public class Systems {
+    private  String systemName;
+    private  String systemNo;
+
+    public Systems() {
+    }
+
+    public String getSystemName() {
+        return systemName;
+    }
+
+    public void setSystemName(String systemName) {
+        this.systemName = systemName;
+    }
+
+    public String getSystemNo() {
+        return systemNo;
+    }
+
+    public void setSystemNo(String systemNo) {
+        this.systemNo = systemNo;
+    }
+}
--- app/src/main/java/com/falconssoft/onlinetechsupport/adapterGridEngineer.java
+++ app/src/main/java/com/falconssoft/onlinetechsupport/adapterGridEngineer.java
@@ -1,21 +1,33 @@
 package com.falconssoft.onlinetechsupport;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.falconssoft.onlinetechsupport.Modle.EngineerInfo;
 
 import java.util.List;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
+import static android.view.LayoutInflater.from;
+
 public class adapterGridEngineer extends BaseAdapter {
     Context context;
-    List<String> list_engineer;
+    List<EngineerInfo> list_engineer;
+    private static String selectedPostionName="";
     adapterGridEngineer(Context context,List list_engineer){
         this.context=context;
         this.list_engineer=list_engineer;
+
 
     }
     @Override
@@ -34,17 +46,38 @@ public class adapterGridEngineer extends BaseAdapter {
     }
 
     @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
+    public View getView(final int i, View view, final ViewGroup viewGroup) {
         View myView = View.inflate(context, R.layout.item_rows,null);
-        TextView green,textView_Nmae;
+        final TextView textView_Nmae;
         CircleImageView profile_image;
-//        green = myView.findViewById(R.id.green);
+        final RelativeLayout engineerLayout;
+        engineerLayout= myView.findViewById(R.id.engineerLayout);
         textView_Nmae = myView.findViewById(R.id.textView_Nmae);
-        profile_image= myView.findViewById(R.id.profile_image);
+        profile_image = myView.findViewById(R.id.profile_image);
+        textView_Nmae.setText(list_engineer.get(i).getName());
+        textView_Nmae.setTag(i);
+
+        textView_Nmae.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int value = (Integer)((TextView)v).getTag();
+
+                for(int i=0;i<viewGroup.getChildCount();i++)
+                {
+                    View c = viewGroup.getChildAt(i);
+                    c.findViewById(R.id.engineerLayout).setBackgroundColor(context.getResources().getColor(R.color.layer1));
+
+                }
+                engineerLayout.setBackgroundColor(context.getResources().getColor(R.color.layer4));
+                selectedPostionName=textView_Nmae.getText().toString();
+                Log.e("selectedPostionName",""+selectedPostionName);
+
+            }
+        });
+
+
         if(i%2!=0)
         {
-//           green.setBackgroundResource(R.drawable.red_circle);
-            textView_Nmae.setText("Eng Abeer Hayari");
             profile_image.setImageResource(R.drawable.call_center_gir);
         }
         return myView;
